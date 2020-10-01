6c2f70da02c6b84d1b2ca0953e1f11da0ad362e8
Aashish <aashish@pop-os.localdomain>
2020-10-01 21:49:04 +0545

    Added subscription

--- contenthub/urls.py
+++ contenthub/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.hompage, name='homepage'),
-    path('sharemarket/', views.sharemarket, name='share'),
-    path('politics/', views.politics, name='politics'),
-    path('gadgets/', views.gadgets, name='gadgets'),
-    path('sports/', views.sports, name='sports'),
-    path('jobs/', views.jobs, name='jobs'),
+    path('contents/', views.contents, name='contents'),
 ]
--- contenthub/views.py
+++ contenthub/views.py
@@ -16,84 +16,84 @@ from bs4 import BeautifulSoup
 
 sites = [
     "https://www.sharesansar.com/category/latest",
-    "https://kathmandupost.com/politics",
     "https://www.gsmarena.com/news.php3",
     "https://kathmandupost.com/sports",
+    "https://kathmandupost.com/politics",
     "https://merojob.com/search/",
     ]
 
-home_data = []
-
-def sharemarket(request):
-    doc = requests.get(sites[0])
-    soup = BeautifulSoup(doc.content, "html5lib")
-    data = []
-    blocks = soup.find_all('div', {'class':'featured-news-list'})
-    for block in blocks:
-        headline = block.findChildren()[5].text
-        image = block.findChildren()[2].get('src')
-        readmore = block.findChildren()[1].get('href')
-        data.append([headline, image, readmore])
-    home_data.append(data)
-    return render(request, './../templates/contents.html', {"data": data})
-
-def politics(request):
-    doc = requests.get(sites[1])
-    soup = BeautifulSoup(doc.content, "html5lib")
-    data = []
-    blocks = soup.find_all('article', {'class':'article-image'})
-    for block in blocks:
-        headline = block.findChildren()[5].text
-        image_src = block.findChildren()[3].get('data-src')
-        image = re.split('[=|&]', image_src)[1]
-        readmore ="https://kathmandupost.com" + block.findChildren()[2].get('href')
-        data.append([headline, image, readmore])
-    home_data.append(data)
-    return render(request, './../templates/contents.html', {"data": data})
-
-def gadgets(request):
-    doc = requests.get(sites[2])
-    soup = BeautifulSoup(doc.content, "html5lib")
-    data = []
-    blocks = soup.find_all('div', {'class':'news-item'})
-    for block in blocks:
-        headline = block.findChildren()[4].text
-        image = block.findChildren()[2].get('src')
-        readmore ="https://www.gsmarena.com/" + block.findChildren()[1].get('href')
-        data.append([headline, image, readmore])
-    home_data.append(data)
-    return render(request, './../templates/contents.html', {"data": data})
-
-def sports(request):
-    doc = requests.get(sites[3])
-    soup = BeautifulSoup(doc.content, "html5lib")
-    data = []
-    blocks = soup.find_all('article', {'class':'article-image'})
-    for block in blocks:
-        headline = block.findChildren()[5].text
-        image_src = block.findChildren()[3].get('data-src')
-        image = re.split('[=|&]', image_src)[1]
-        readmore ="https://kathmandupost.com" + block.findChildren()[2].get('href')
-        data.append([headline, image, readmore])
-    home_data.append(data)
-    return render(request, './../templates/contents.html', {"data": data})
-
-def jobs(request):
-    doc = requests.get(sites[4])
-    soup = BeautifulSoup(doc.content, "html5lib")
-    data = []
-    blocks = soup.find_all('div', {'class':'hover-shadow'})
-    for block in blocks:
-        readmore ="https://merojob.com" +   block.findChildren()[4].find('a').get('href')
-        headline = block.findChildren()[4].find('h1').text
-        organization = block.findChildren()[5].find('meta').get('content')
-        skills = block.findChildren()[28].text
-        image ="https://merojob.com" +  block.findChildren()[5].find('img').get('src')
-        raw_body = organization + " (" + skills + ")"
-        body = re.sub(' +', ' ', raw_body)
-        data.append([headline, image, readmore, body])
-    home_data.append(data)
-    return render(request, './../templates/contents.html', {"data": data})
-
 def hompage(request):
-    return render(request, 'index.html',{"data": home_data})
+    return render(request, 'index.html',{})
+
+
+def contents(request):
+    if request.method == "POST":
+        content = request.POST["dropdown"]
+
+        if content == "Sharemarket":
+            doc = requests.get(sites[0])
+            soup = BeautifulSoup(doc.content, "html5lib")
+            data = []
+            blocks = soup.find_all('div', {'class':'featured-news-list'})
+            for block in blocks:
+                headline = block.findChildren()[5].text
+                image = block.findChildren()[2].get('src')
+                readmore = block.findChildren()[1].get('href')
+                data.append([headline, image, readmore])
+            return render(request, './../templates/contents.html', {"data": data, "content": content })
+
+        elif content == "Gadgets":
+            doc = requests.get(sites[1])
+            soup = BeautifulSoup(doc.content, "html5lib")
+            data = []
+            blocks = soup.find_all('div', {'class':'news-item'})
+            for block in blocks:
+                headline = block.findChildren()[4].text
+                image = block.findChildren()[2].get('src')
+                readmore ="https://www.gsmarena.com/" + block.findChildren()[1].get('href')
+                data.append([headline, image, readmore])
+            return render(request, './../templates/contents.html', {"data": data, "content": content })
+
+        if content == "Sports":
+            doc = requests.get(sites[2])
+            soup = BeautifulSoup(doc.content, "html5lib")
+            data = []
+            blocks = soup.find_all('article', {'class':'article-image'})
+            for block in blocks:
+                headline = block.findChildren()[5].text
+                image_src = block.findChildren()[3].get('data-src')
+                image = re.split('[=|&]', image_src)[1]
+                readmore ="https://kathmandupost.com" + block.findChildren()[2].get('href')
+                data.append([headline, image, readmore])
+            return render(request, './../templates/contents.html', {"data": data, "content": content })
+
+        elif content == "Politics":
+            doc = requests.get(sites[3])
+            soup = BeautifulSoup(doc.content, "html5lib")
+            data = []
+            blocks = soup.find_all('article', {'class':'article-image'})
+            for block in blocks:
+                headline = block.findChildren()[5].text
+                image_src = block.findChildren()[3].get('data-src')
+                image = re.split('[=|&]', image_src)[1]
+                readmore ="https://kathmandupost.com" + block.findChildren()[2].get('href')
+                data.append([headline, image, readmore])
+            return render(request, './../templates/contents.html', {"data": data, "content": content })
+
+        else:
+            doc = requests.get(sites[4])
+            soup = BeautifulSoup(doc.content, "html5lib")
+            data = []
+            blocks = soup.find_all('div', {'class':'hover-shadow'})
+            for block in blocks:
+                readmore ="https://merojob.com" +   block.findChildren()[4].find('a').get('href')
+                headline = block.findChildren()[4].find('h1').text
+                organization = block.findChildren()[5].find('meta').get('content')
+                skills = block.findChildren()[28].text
+                image ="https://merojob.com" +  block.findChildren()[5].find('img').get('src')
+                raw_body = organization + " (" + skills + ")"
+                body = re.sub(' +', ' ', raw_body)
+                data.append([headline, image, readmore, body])
+            return render(request, './../templates/contents.html', {"data": data, "content": content })
+        
+
