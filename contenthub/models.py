988364abe04012fa5658a345a9e50a3b1f52db0f
Aashish <aashish@pop-os.localdomain>
2020-10-03 16:45:50 +0545

    Models added

--- contenthub/models.py
+++ contenthub/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+    headline = models.CharField(max_length=250)
+    body = models.CharField(max_length=250)
+    image = models.URLField(max_length = 250) 
+    link = models.URLField(max_length = 250) 
+    tag = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.headline + "(" + self.tag + ")"
