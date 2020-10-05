6faa3cdda399ebc173ca9c6c4f33fae387db4f62
Aashish <aashish@pop-os.localdomain>
2020-10-05 19:12:13 +0545

    Subscription added to api

--- contenthub/apiviews.py
+++ contenthub/apiviews.py
@@ -1,14 +1,15 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Content
 from .serializers import ContentSerializer
 
-
-class ContentList(generics.ListCreateAPIView):
-    queryset = Content.objects.all()
-    serializer_class = ContentSerializer
-
-
-class ContentDetail(generics.RetrieveDestroyAPIView):
-    queryset = Content.objects.all()
-    serializer_class = ContentSerializer
+@api_view(['GET', 'POST'])
+def ContentList(request):
+    if request.method == 'POST':
+        category = request.POST["dropdown"]
+        data = Content.objects.filter(tag=category)
+        serializer = ContentSerializer(data, many=True)
+        return Response(serializer.data)
+    return Response({"message": "No data found"})
--- contenthub/urls.py
+++ contenthub/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
 
-from .apiviews import ContentList, ContentDetail
+from .apiviews import ContentList
 
 urlpatterns = [
     path('', views.hompage, name='homepage'),
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path("contentapi/", ContentList.as_view(), name="content_list"),
+    path("contentapi/", ContentList, name="content_list"),
     path('contents/', views.contents, name='contents'),
-    path("contentapi/<int:pk>/", ContentDetail.as_view(), name="content_detail")
 ]
