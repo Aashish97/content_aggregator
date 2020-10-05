8c3c745c68a130bb1ce90fda6366cfc368797fb8
Aashish <aashish@pop-os.localdomain>
2020-10-05 15:51:02 +0545

    Bootstrap added

--- contenthub/forms.py
+++ contenthub/forms.py
@@ -7,4 +7,14 @@ from django import forms
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+		fields = ['username', 'email']
+
+		widgets ={
+			'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+			'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+		}
+	
+	def __init__(self, *args, **kwargs):
+		super(CreateUserForm, self).__init__(*args, **kwargs)
+		self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':"Password"})
+		self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': "Repeat your password"})
