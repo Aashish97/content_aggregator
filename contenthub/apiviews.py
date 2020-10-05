2d2fd3b694131ade223ae6c4411174e7cfcaa4f7
Aashish <aashish@pop-os.localdomain>
2020-10-05 17:42:21 +0545

    Using DRF generic views

--- contenthub/apiviews.py
+++ contenthub/apiviews.py
@@ -1,19 +1,14 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import Content
-from  .serializers import ContentSerializer
-
-class ContentList(APIView):
-    def get(self, request):
-        contents = Content.objects.all()
-        data = ContentSerializer(contents, many=True).data
-        return Response(data)
+from .serializers import ContentSerializer
 
 
-class ContentDetail(APIView):
-    def get(self, request, pk):
-        content = get_object_or_404(Content, pk=pk)
-        data = ContentSerializer(content).data
-        return Response(data)
+class ContentList(generics.ListCreateAPIView):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+
+
+class ContentDetail(generics.RetrieveDestroyAPIView):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
--- contenthub/urls.py
+++ contenthub/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('contents/', views.contents, name='contents'),
     path("contentapi/", ContentList.as_view(), name="content_list"),
+    path('contents/', views.contents, name='contents'),
     path("contentapi/<int:pk>/", ContentDetail.as_view(), name="content_detail")
 ]
